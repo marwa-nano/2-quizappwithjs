11b2b9d0bacdb0f2b22d11de68a61375a30eb940
marwa-nano <marwa.nanodegree@gmail.com>
2024-01-16 18:33:34 +0200

    createBullets

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const answerC = document.getElementById("c-answer");
 const answerD = document.getElementById("d-answer");
 
 const submitBtn = document.querySelector(".submit-btn");
-const quizApp = document.querySelector(".quiz-app-content")
+const quizApp = document.querySelector(".quiz-app-content");
+const bulletsContainer = document.querySelector(".bullets");
 
 let currentQuiz = 0;
 let quizObj;
@@ -24,10 +25,10 @@ function getQuiz(){
     quizRequest.onreadystatechange = function(){
         if (this.readyState === 4 && this.status === 200){
          quizObj = JSON.parse(quizRequest.responseText);
-          const quizCount = quizObj.length;
-          qCount.innerText  = quizCount;
+          const quizCount = quizObj.length;          
+          createBullets(quizCount);
           
-          const currentQuizData = quizObj[currentQuiz]
+          const currentQuizData = quizObj[currentQuiz];
           loadQuiz(currentQuizData);
         }
     }  
@@ -40,6 +41,28 @@ function loadQuiz(quizData){
     answerB.innerText = quizData.b;
     answerC.innerText = quizData.c;
     answerD.innerText = quizData.d;
+}
+
+function createBullets(count){
+    qCount.innerText = count;
+    for (let i= 0; i< count ; i++){
+        let bullet = document.createElement('span');
+       
+        if (i == 0 ) {
+            bullet.className = "active";
+        }
+        bulletsContainer.appendChild(bullet);
+    }
+
+}
+
+function setActiveSpan(){
+    const spanEls = document.querySelectorAll(".bullets span");
+    spanEls.forEach((spanEl,index)=>{
+        if (currentQuiz === index){
+            spanEl.className = "active";
+        }
+    })
 }
 
 function deselectAnswers(){
@@ -72,11 +95,16 @@ submitBtn.addEventListener("click",()=>{
         if(currentQuiz < quizObj.length){
             const nextQuizData = quizObj[currentQuiz];
             loadQuiz(nextQuizData);
+            setActiveSpan();
         } else {
             submitBtn.remove();
+            bulletsContainer.remove();
             quizApp.innerHTML =
             `<h2>you answered  ${score} correctly from ${quizObj.length} questions. </h2>
-            <button onclick="location.reload()">Reload</button>`;
+            <br>
+            <button onclick="location.reload()">Reload</button>
+            <br>
+            `;
             
         }
     }   
